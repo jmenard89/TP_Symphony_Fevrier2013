c4ac4740509caeb29ee260ba59c1bad2532c529b
Yahnn <Yahnn@172.16.12.6>
2013-02-15 14:22:47 +0100

    Creation des bundles Suppression repertoire ACME + references

--- src/GestionDeProjet/ClientBundle/Controller/DefaultController.php
+++ src/GestionDeProjet/ClientBundle/Controller/DefaultController.php
@@ -6,8 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function connexionAction()
     {
-        return $this->render('GestionDeProjetClientBundle:Default:index.html.twig', array('name' => $name));
+        // On récupère la requête
+         $request = $this->get('request');
+ 
+        // On récupère nos paramètres [login et mdp]
+        $login = $request->request->get('form_login');
+        $mdp = $request->request->get('form_mdp');
+        
+        // On regarde qu'el est l'id de la personne connecté
+        //$sql = $this->getDoctrine()->getRepository($persistentObjectName)
+        // Maintenant que nous avons l'id on l'insert dans une session
+       
+        return $this->render('GestionDeProjetClientBundle:Default:connexion.html.twig',array("login"=>$login));
     }
+    
+          
 }
